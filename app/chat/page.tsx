931053b9e9385b3f49d8b6cd2ee7c6694e606fab
Daniel Sadiq <danielsadiq93@gmail.com>
2025-10-10 23:41:30 +0100

    working v4

--- app/chat/page.tsx
+++ app/chat/page.tsx
@@ -1,3 +1,5 @@
+import { getUserChats } from "@/lib/dataServices";
+
 export default function page() {
   return (
     <div className="px-20 py-12 h-screen">
@@ -17,16 +19,22 @@ function Header() {
   );
 }
 
-function Body() {
+async function Body() {
+  const chats = await getUserChats("daniel@example.com");
+  console.log(chats)
+
   return (
     <main className="mt-12 p-6">
-      <div className="flex items-center gap-4">
+      {chats?.map(chat => (
+        <div className="flex items-center gap-4">
         <div className="rounded-full w-[3rem] h-[3rem] border-2 border-black"></div>
         <div>
-          <h2 className="font-bold">Daniel Sadiq</h2>
+          <h2 className="font-bold">{chat.name}</h2>
           <p>The weather will be perfect</p>
         </div>
       </div>
+      ))}
+      
     </main>
   );
 }
