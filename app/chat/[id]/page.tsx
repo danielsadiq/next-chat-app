b389b5a21e416b6794878952edb81f4a1f304d98
Daniel Sadiq <danielsadiq93@gmail.com>
2025-10-12 04:48:45 +0100

    v1

--- app/chat/[id]/page.tsx
+++ app/chat/[id]/page.tsx
@@ -1,10 +1,57 @@
-export default function page() {
+import { getMessages } from "@/lib/dataServices";
+import Body from "./Body";
+// import { subscribeToMessages } from "@/lib/services";
+// import { useEffect, useState } from "react";
+
+export default function Page({params}: {params: {id:string}}) {
+  const {id: convoId} = params
+  // const [messages, setMessages] = useState<any[]>([]);
+  // const [loading, setLoading] = useState(true);
+  console.log(convoId)
+
+  // useEffect(() => {
+  //   // 1️⃣ Fetch existing messages
+  //   (async () => {
+  //     const msgs = await getMessages(convoId);
+  //     setMessages(msgs);
+  //     setLoading(false);
+  //   })();
+
+  //   // 2️⃣ Subscribe to realtime updates
+  //   const unsubscribe = subscribeToMessages(convoId, (newMessage) => {
+  //     setMessages((prev) => [...prev, newMessage]);
+  //   });
+
+  //   // 3️⃣ Cleanup
+  //   return unsubscribe;
+  // }, [convoId]);
+
+  // if (loading) return <p>Loading chat...</p>;
+
   return (
-    <div>
+    <div className="flex flex-col gap-2 p-4">
       <Header/>
+      <Body convoId={convoId} />
+      {/* {messages.map((msg) => (
+        <div key={msg.id} className="p-2 rounded bg-gray-100">
+          <strong>{msg.sender?.display_name || "Unknown"}:</strong> {msg.content}
+        </div>
+      ))} */}
     </div>
-  )
+  );
 }
+
+
+
+// export default function page({params}: {params: {convoId:string}}) {
+//   const convos = getMessages(params.convoId);
+//   return (
+//     <div>
+//       <Header/>
+//       <div></div>
+//     </div>
+//   )
+// }
 
 function Header() {
   return (
@@ -15,3 +62,4 @@ function Header() {
     </div>
   );
 }
+
