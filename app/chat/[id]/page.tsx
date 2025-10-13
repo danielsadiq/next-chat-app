00f41a8e1d39b5dbd882d8bb49eb97ae34336882
Daniel Sadiq <danielsadiq93@gmail.com>
2025-10-13 01:59:10 +0100

    conversation page style

--- app/chat/[id]/page.tsx
+++ app/chat/[id]/page.tsx
@@ -1,65 +1,25 @@
-import { getMessages } from "@/lib/dataServices";
 import Body from "./Body";
-// import { subscribeToMessages } from "@/lib/services";
-// import { useEffect, useState } from "react";
 
-export default function Page({params}: {params: {id:string}}) {
-  const {id: convoId} = params
-  // const [messages, setMessages] = useState<any[]>([]);
-  // const [loading, setLoading] = useState(true);
-  console.log(convoId)
+type PageProps = {
+  params: Promise<{ id: string }>;
+};
 
-  // useEffect(() => {
-  //   // 1️⃣ Fetch existing messages
-  //   (async () => {
-  //     const msgs = await getMessages(convoId);
-  //     setMessages(msgs);
-  //     setLoading(false);
-  //   })();
-
-  //   // 2️⃣ Subscribe to realtime updates
-  //   const unsubscribe = subscribeToMessages(convoId, (newMessage) => {
-  //     setMessages((prev) => [...prev, newMessage]);
-  //   });
-
-  //   // 3️⃣ Cleanup
-  //   return unsubscribe;
-  // }, [convoId]);
-
-  // if (loading) return <p>Loading chat...</p>;
-
+export default async function Page({ params }: PageProps) {
+  const { id } = await params;
   return (
-    <div className="flex flex-col gap-2 p-4">
+    <div className="max-w-lg md:max-w-xl mx-auto my-6">
       <Header/>
-      <Body convoId={convoId} />
-      {/* {messages.map((msg) => (
-        <div key={msg.id} className="p-2 rounded bg-gray-100">
-          <strong>{msg.sender?.display_name || "Unknown"}:</strong> {msg.content}
-        </div>
-      ))} */}
+      <Body convoId={id} />
     </div>
   );
 }
 
-
-
-// export default function page({params}: {params: {convoId:string}}) {
-//   const convos = getMessages(params.convoId);
-//   return (
-//     <div>
-//       <Header/>
-//       <div></div>
-//     </div>
-//   )
-// }
-
 function Header() {
   return (
-    <div className="flex justify-between">
-      <div className="rounded-full w-[3rem] h-[3rem]">{"<"}</div>
+    <div className="flex justify-between items-center">
+      <div className="rounded-full w-[3rem] h-[3rem] flex items-center">{"<"}</div>
       <h1 className="text-3xl font-bold">Daniel</h1>
       <div className="rounded-full w-[3rem] h-[3rem] border-2 border-black"></div>
     </div>
   );
 }
-
