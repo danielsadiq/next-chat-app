fc6f62681748a5510e342dfab40354b196809ca9
Daniel Sadiq <danielsadiq93@gmail.com>
2025-10-13 15:11:52 +0100

    context provider for user app wide

--- app/chat/layout.tsx
+++ app/chat/layout.tsx
@@ -1,8 +1,29 @@
-import { UserProvider } from "@/context/UserContext";
+"use client"
+
+import { UserProvider, useUser } from "@/context/UserContext";
+import { getUser } from "@/lib/api-users";
+import { useEffect } from "react";
+
+const email = "daniel@example.com"
 
 export default function ChatLayout({ children }: { children: React.ReactNode }) {
+  function ChatInitializer() {
+  const { setUser } = useUser();
+
+  useEffect(() => {
+    async function getMyUser(){
+      const data = await getUser(email);
+      setUser(data)
+    }
+    getMyUser();
+  }, [setUser]);
+
+  return null; // doesn’t render anything visible
+}
+
   return (
     <UserProvider>
+      <ChatInitializer/>
       <div className="flex h-screen">
         {children}
       </div>
