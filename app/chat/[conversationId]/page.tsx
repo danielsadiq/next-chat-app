9b619a0ab1e4bdafc6fac9ec02eda8281bc787b5
Daniel Sadiq <danielsadiq93@gmail.com>
2025-12-07 22:58:23 +0100

    page update

--- app/chat/[conversationId]/page.tsx
+++ app/chat/[conversationId]/page.tsx
@@ -5,16 +5,21 @@ import Spinner from "@/components/Spinner";
 import Body from "./components/Body";
 
 type PageProps = {
-  params: Promise<{ conversationId: string }>;
+  params: Promise<{ conversationId: string}>;
+  searchParams: Promise<{ name?: string; image?: string }>;
+  // searchParams: { [key: string]: string | string[] | undefined };
 };
 
-export default async function Page({ params }: PageProps) {
+export default async function Page({ params, searchParams }: PageProps) {
   const { conversationId } = await params;
+  const { name, image } = await searchParams;
+  const displayName = name ?? "Unknown";
+  const displayImage = image ?? "";
 
   return (
-    <div className="max-w-lg md:max-w-xl mx-auto flex flex-col min-h-screen">
-      <Header />
-      <div className="flex-1 flex items-center justify-center">
+    <div className="max-w-lg md:max-w-xl mx-auto flex flex-col h-full">
+      <Header name={displayName} image={displayImage} />
+      <div className="flex flex-grow items-center justify-center">
         <Suspense
           fallback={
             <Spinner/>
