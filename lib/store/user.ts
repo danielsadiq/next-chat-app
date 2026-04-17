c238b3c761cb1565f056827c1f0beda347fe39e0
Daniel Sadiq <danielsadiq93@gmail.com>
2026-04-17 22:45:30 +0100

    user store initiate

---
+++ lib/store/user.ts
@@ -0,0 +1,13 @@
+// store.ts
+import { User } from '@supabase/supabase-js';
+import { create } from 'zustand';
+
+// Define types for state & actions
+interface UserState {
+  user: User | undefined,
+}
+
+// Create store using the curried form of `create`
+export const useBearStore = create<UserState>()((set) => ({
+  user: undefined,
+}))
