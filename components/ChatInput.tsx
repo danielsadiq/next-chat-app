3206bbf41b8aa41b4f43386613075f3f4ad4a5e1
Daniel Sadiq <danielsadiq93@gmail.com>
2026-04-18 18:54:34 +0100

    chat update

--- components/ChatInput.tsx
+++ components/ChatInput.tsx
@@ -4,12 +4,15 @@ import { Input } from "./ui/input";
 export default function ChatInput() {
   function handleSendMessage(message:string){
     alert(message);
+    // call to supabase
+    
   }
   return (
     <div className="p-5">
       <Input placeholder="send message" onKeyDown={(e)=> {
         if (e.key === "Enter"){
           handleSendMessage(e.currentTarget.value);
+          e.currentTarget.value = "";
         }
       }} />
     </div>
