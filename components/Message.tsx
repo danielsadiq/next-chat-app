2c824ab00c9379cc4e1e7b4b5f28f3ea838b9f97
Daniel Sadiq <danielsadiq93@gmail.com>
2026-04-22 12:56:22 +0100

    dropdown menu for edit and delete add

--- components/Message.tsx
+++ components/Message.tsx
@@ -1,7 +1,19 @@
 import { IMessage } from "@/lib/store/messages";
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 import Image from "next/image";
+import { Ellipsis } from "lucide-react";
+import { useUser } from "@/lib/store/user";
 
 function Message({ message }: { message: IMessage }) {
+  const user = useUser(state => state.user)
   return (
     <div className="flex gap-2">
       <div>
@@ -14,15 +26,35 @@ function Message({ message }: { message: IMessage }) {
         />
       </div>
       <div className="flex-1">
-        <div className="flex items-center gap-1">
-          <h1 className="font-bold">{message.users?.user_name}</h1>
-          <h1 className="text-sm text-gray-400">
-            {new Date(message.created_at).toDateString()}
-          </h1>
+        <div className="flex items-center justify-between">
+          <div className="flex items-center gap-1">
+            <h1 className="font-bold">{message.users?.user_name}</h1>
+            <h1 className="text-sm text-gray-400">
+              {new Date(message.created_at).toDateString()}
+            </h1>
+          </div>
+          {user && message.users?.id === user.id && <MessageMenu/>}
         </div>
         <p className="text-gray-600">{message.text}</p>
       </div>
     </div>
+  );
+}
+
+function MessageMenu() {
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant="outline"><Ellipsis/></Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent>
+        <DropdownMenuGroup>
+          <DropdownMenuLabel>Action</DropdownMenuLabel>
+          <DropdownMenuItem>Edit</DropdownMenuItem>
+          <DropdownMenuItem>Delete</DropdownMenuItem>
+        </DropdownMenuGroup>
+      </DropdownMenuContent>
+    </DropdownMenu>
   );
 }
 
