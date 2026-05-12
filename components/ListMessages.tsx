e046229f18670badaa35686331399afc1a0433c9
Daniel Sadiq <danielsadiq93@gmail.com>
2026-05-12 11:13:37 +0100

    scroll changes

--- components/ListMessages.tsx
+++ components/ListMessages.tsx
@@ -4,12 +4,19 @@ import { IMessage, useMessageStore } from "@/lib/store/messages";
 import Message from "./Message";
 import { DeleteAlert, EditAlert } from "./MessageAction";
 import { createClient } from "@/lib/supabase/client";
-import { useEffect, useRef } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
+import { ArrowDown } from "lucide-react";
 import { toast } from "sonner";
 
 function ListMessages() {
+  const {
+    messages,
+    addMessage,
+    optimisticUpdateMessage,
+    optimisticDeleteMessage,
+  } = useMessageStore((state) => state);
   const scrollRef = useRef<HTMLDivElement>(null); // Create the Ref
-  const { messages, addMessage, optimisticUpdateMessage, optimisticDeleteMessage } = useMessageStore((state) => state);
+  const [userScrolled, setUserScrolled] = useState(false);
   const supabase = createClient();
   useEffect(() => {
     const channel = supabase
@@ -42,7 +49,7 @@ function ListMessages() {
               }
             case "UPDATE":
               optimisticUpdateMessage(newRecord.id, newRecord.text);
-              break
+              break;
             case "DELETE":
               optimisticDeleteMessage(oldRecord.id);
               break;
@@ -67,17 +74,48 @@ function ListMessages() {
       scrollRef.current.scrollIntoView({ behavior: "smooth" });
     }
   }, [messages]); // Trigger this every time the messages array updates
+
+  const handleOnScroll = () => {
+    const scrollContainer = scrollRef.current;
+    if (scrollContainer) {
+      const isScroll =
+        scrollContainer.scrollTop <
+        scrollContainer.scrollHeight - scrollContainer.clientHeight - 10;
+      console.log(isScroll);
+      setUserScrolled(isScroll);
+    }
+  };
+  const scrollToBottom = useCallback(() => {
+    scrollRef.current?.scrollTo({
+      top: scrollRef.current.scrollHeight,
+      behavior: "smooth", // Adds that nice sliding animation
+    });
+  }, []);
   return (
-    <>
-      <div className="space-y-7 overflow-y-auto">
+    <div className="flex flex-col overflow-hidden">
+      <div
+        className="space-y-7 overflow-y-auto"
+        ref={scrollRef}
+        onScroll={handleOnScroll}
+      >
         {messages.map((value, index) => {
           return <Message key={index} message={value} />;
         })}
-        <div ref={scrollRef} />
+        <div />
       </div>
+      {userScrolled && (
+        <div className="absolute bottom-20 right-1/2">
+          <div
+            className="w-10 h-10 bg-blue-400 rounded-full flex items-center justify-center mx-auto border cursor-pointer hover:scale-110 transition-all"
+            onClick={scrollToBottom}
+          >
+            <ArrowDown />
+          </div>
+        </div>
+      )}
       <DeleteAlert />
       <EditAlert />
-    </>
+    </div>
   );
 }
 
