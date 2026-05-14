26d10cdd42f469eaceb11322142c71b76b546f2f
Daniel Sadiq <danielsadiq93@gmail.com>
2026-05-14 16:40:12 +0100

    pagination function add

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,12 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function getFromAndTo(page:number, itemPerPage:number){
+  let from = page * itemPerPage;
+  const to = from + itemPerPage;
+  if (page > 0){
+    from +=1;
+  }
+  return {from, to};
+}
